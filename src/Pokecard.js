4f3d322b93319d3b6250f925bcec50b027a812f0
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-25 21:53:32 +0200

    bind(this) - method vs arrow function property

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,13 +5,23 @@ const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 const padToThree = number => number > 999 ? number : `00${number}`.slice(-3)
 
 class Pokecard extends Component {
+    state = {
+        name: '?'
+    }
+
+    // Does NOT require to bind(this)
+    clickHandler = event => this.setState({ name: this.props.name })
+
+    // Requires to bind(this)
+    // clickHandler(event) { this.setState({ name: this.props.name }) }
+
     render() {
         const { id, name, type, exp } = this.props
         const imgSrc = `${POKE_API}${padToThree(id)}.png`
 
         return (
-            <div className="Pokecard">
-                <h1 className="Pokecard-title">{name}</h1>
+            <div className="Pokecard" onClick={this.clickHandler}>
+                <h1 className="Pokecard-title">{this.state.name}</h1>
                 <div className="Pokecard-image">
                     <img src={imgSrc} alt={name} />
                 </div>
